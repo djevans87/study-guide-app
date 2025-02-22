87a1267133dfe717ec01be28a4c15db42c08425d
Darian <84196221+djevans87@users.noreply.github.com>
2025-02-22 17:43:05 -0500

    implemented usestate store and reducers

---
+++ src/state_management/reducers/currentUserSlice.jsx
@@ -0,0 +1,44 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const currentUserSlice = createSlice({
+    name: 'currentUser',
+    initialState:{
+        username: '',
+        password: '',
+        isLoggedIn: false,
+        firstName: '',
+        lastName: '',
+        email: '',
+        phoneNumber: '',
+    },
+    reducers: {
+        login: (state, action) => {
+            const { username, password } = action.payload;
+            state.username = username;
+            state.password = password;
+            state.isLoggedIn = true;
+        },
+        logout: (state) => {
+            state.username = '';
+            state.password = '';
+            state.isLoggedIn = false;
+            state.firstName = '';
+            state.lastName = '';
+            state.email = '';
+            state.phoneNumber = '';
+        },
+        register: (state, action) => {
+            const {firstName, lastName, email, phoneNumber, username, password} = action.payload;
+            state.isLoggedIn = true;
+            state.email = email;
+            state.password = password;
+            state.phoneNumber = phoneNumber;
+            state.firstName = firstName;
+            state.lastName = lastName;
+            state.username = username;
+        }
+    },
+});
+
+export const { register, login, logout } = currentUserSlice.actions;
+export default currentUserSlice.reducer;
